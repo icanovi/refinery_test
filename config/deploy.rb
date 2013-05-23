407de67f4d7a8af3151d75f0804a33e27d969d8e
Ignacio Canovi <ignacio.canovi@729Solutions.com>
2013-05-23 15:29:20 -0300

    - Initial Committ

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+set :application, "Refinery CMS Test"
+
+set :scm, :git 
+set :repository,  "git@github.com:icanovi/refinery_test.git"
+set :scm_passphrase, ""
+
+set :user, "nacho"
+
+set :deploy_to "/home/nacho/refinery"
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RefineryApp::Application.config.secret_token = 'add581b0c3b4814798cedf22e1bd8c2ae4052b5d4cf0e393b0913a9282b810098785593802909a0824cba4374bd3b8de7864c32f0a858e7c1082001cfd221af1'
